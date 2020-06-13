37ca407c0706320a579b14409d637e40261e0652
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2020-06-13 14:12:15 +0300

    Updating code

--- hex.js
+++ hex.js
@@ -1,1 +1,13 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]; //This array is going to be used to generate random hex colors
+// #f15025 Format of hex colors
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+let hexColor = '#';
+for(let i = 0; i < 6; i++) {
+	hexColor += hex[0] //Here I'm hardcoding, to generate a black color.
+}
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor;
+})
