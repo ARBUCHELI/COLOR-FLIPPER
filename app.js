193f218a4ff3e668299722c6023acbff000fd5ea
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2020-06-13 14:54:50 +0300

    Updating file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const colors = ["green", "red", "yellow", "blue"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
