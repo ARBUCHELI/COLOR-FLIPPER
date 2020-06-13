9877697173ac5dd814bbf6ddaf04911abfdd6ab3
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2020-06-13 12:17:40 +0300

    Updating

--- test.js
+++ test.js
@@ -3,7 +3,13 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
 	// get a random number between 0 and 3
-	const randomNumber = 2; //But here I'm hardcodig this
+	const randomNumber = getRandomNumber(); //But here I'm hardcodig this
 	document.body.style.backgroundColor = colors[randomNumber];
 	color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+	return Math.floor(Math.random()*colors.length); /*Because random only generates
+	 numbers between a and 1.*/
+	 /*We need Math.floor in order to round the generated numbers to integers*/
+}
